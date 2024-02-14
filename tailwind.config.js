a611065c6d52a17948cd16068960aa1644268176
juwamardott <juwamardood@gmail.com>
2024-02-14 13:54:12 +0800

    upload wedding

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        beleza: ["Belleza"],
+        italiano: ["Italianno"],
+      },
+      colors: {
+        hijau: ["#878973"],
+        hijaukuning: ["#7b7547"],
+      },
+    },
+  },
+  plugins: [],
+};
